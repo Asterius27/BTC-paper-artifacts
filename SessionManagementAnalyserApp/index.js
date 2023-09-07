11d06b84dd05cd8851f65f5d5b57905ea6a7b9d9
Asterius27 <878352@stud.unive.it>
2023-09-07 17:11:15 +0200

    .

--- SessionManagementAnalyserApp/index.js
+++ SessionManagementAnalyserApp/index.js
@@ -8,11 +8,10 @@ const octokit = new Octokit({ auth: process.env.TOKEN });
 // /search/repositories does not have limitations (for the results) but doesn't allow to search for keywords in files or the existence of specific files.
 // It allows to filter by language (automatically detected by github) and it allows to filter using topics, but they are user defined.
 // when having millions of results both the web ui and the api aren't perfect (sometimes it returns more results, other times less results) (as for repositories)
-// web ui and api results don't match when the query is generic (ex. when only filtering by language), with more specific queries the results match.
-// /search/code limits it's result when compared to the web ui (see https://docs.github.com/en/search-github/searching-on-github/searching-code for full list of limitations), but allows to search for stuff inside the files.
+// web ui and api results don't match when the query is generic (ex. when only filtering by language), with more specific queries the results match. (api returns more results)
+// /search/code limits it's result when compared to the web ui (see https://docs.github.com/en/search-github/searching-on-github/searching-code for full list of limitations), but allows to search for stuff inside the files. (returns way less results than web ui)
 // results also differ between requests that are the same but made in different points in time (when using /search/code)
-// TODO what to do? get all python repositories and then filter them locally, maybe using codeql?
-// TODO this reaches the API rate limit, it will take a while to download all of the repositories, maybe add a timer and wait in order to not exceed the rate limit?
+// TODO this reaches the API rate limit, it will take a while to download all of the repositories, maybe add a timer and wait in order to not exceed the rate limit? yes
 // For authenticated requests, you can make up to 30 requests per minute for all search endpoints except for the "Search code" endpoint. The "Search code" endpoint requires you to authenticate and limits you to 10 requests per minute
 let arr = []
 for (let i = 0; ; i++) {
@@ -20,7 +19,7 @@ for (let i = 0; ; i++) {
         headers: {
             'X-GitHub-Api-Version': '2022-11-28',
         },
-        q: '"from flask_login" language:Python', // add fork:true to include forks, should we also include forks?
+        q: '"from flask_login" language:Python', // add fork:true to include forks, should we also include forks? no
         per_page: 100,
         page: i + 1
     });
