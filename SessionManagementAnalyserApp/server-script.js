9b7cf7fa9de0905661048ade054b0f1e32639f90
Asterius27 <878352@stud.unive.it>
2023-10-11 19:26:29 +0200

    server script

---
+++ SessionManagementAnalyserApp/server-script.js
@@ -0,0 +1,45 @@
+import { Octokit } from "octokit";
+import 'dotenv/config';
+import * as fs from 'fs';
+import decompress from "decompress";
+import { execSync } from "child_process";
+import csvParser from 'csv-parser';
+
+const octokit = new Octokit({ auth: process.env.TOKEN });
+const framework = "Flask";
+let i = 0;
+// let skip = [];
+
+if (!fs.existsSync("./repositories")){
+    fs.mkdirSync("./repositories");
+}
+if (!fs.existsSync("./repositories/" + framework)){
+    fs.mkdirSync("./repositories/" + framework);
+}
+
+fs.createReadStream('../flask_repos.csv')
+  .pipe(csvParser())
+  .on('data', (data) => {
+    if (i < 500) {
+        let owner = data.repo_url.split("/")[3];
+        octokit.request('GET /repos/{owner}/{repo}/zipball', {
+            owner: owner,
+            repo: data.repo_name,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        }).then(async (zip) => {
+            fs.appendFileSync("repositories/" + framework + "/" + owner + "_" + data.repo_name + ".zip", Buffer.from(zip.data));
+            try {
+                await decompress('./repositories/' + framework + '/' + owner + "_" + data.repo_name + '.zip', './repositories/' + framework + '/' + owner + "_" + data.repo_name);
+            } catch(e) {
+                console.log(e);
+                // skip.push(owner + "_" + data.repo_name);
+            }
+            fs.unlinkSync(repos_dir + '/repo' + i + '.zip');
+        });
+    }
+    i++;
+}).on('end', () => {
+    console.log("Finished downloading and extracting the repos\n");
+});
