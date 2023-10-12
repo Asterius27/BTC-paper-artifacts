601001ee5ed686973e97bd7e1d9c5d605dff587e
Asterius27 <878352@stud.unive.it>
2023-10-12 15:06:49 +0200

    Update server-script.js

--- SessionManagementAnalyserApp/server-script.js
+++ SessionManagementAnalyserApp/server-script.js
@@ -18,36 +18,39 @@ if (!fs.existsSync("./repositories/" + framework)){
     fs.mkdirSync("./repositories/" + framework);
 }
 
-/* Download and extract the repositories
+// Download and extract the repositories
 fs.createReadStream('../flask_repos.csv')
   .pipe(csvParser())
   .on('data', (data) => {
     if (i < 500) {
         let owner = data.repo_url.split("/")[3];
-        octokit.request('GET /repos/{owner}/{repo}/zipball', {
-            owner: owner,
-            repo: data.repo_name,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        }).then(async (zip) => {
-            fs.appendFileSync("repositories/" + framework + "/" + owner + "_" + data.repo_name + ".zip", Buffer.from(zip.data));
-            try {
-                await decompress('./repositories/' + framework + '/' + owner + "_" + data.repo_name + '.zip', './repositories/' + framework + '/' + owner + "_" + data.repo_name);
-            } catch(e) {
-                console.log(e);
-                // skip.push(owner + "_" + data.repo_name);
-            }
-            fs.unlinkSync("repositories/" + framework + "/" + owner + "_" + data.repo_name + ".zip");
-        });
+        try {
+            octokit.request('GET /repos/{owner}/{repo}/zipball', {
+                owner: owner,
+                repo: data.repo_name,
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            }).then(async (zip) => {
+                fs.appendFileSync("repositories/" + framework + "/" + owner + "_" + data.repo_name + ".zip", Buffer.from(zip.data));
+                try {
+                    await decompress('./repositories/' + framework + '/' + owner + "_" + data.repo_name + '.zip', './repositories/' + framework + '/' + owner + "_" + data.repo_name);
+                } catch(e) {
+                    console.log("Error Caught:\n" + e);
+                    // skip.push(owner + "_" + data.repo_name);
+                }
+                fs.unlinkSync("repositories/" + framework + "/" + owner + "_" + data.repo_name + ".zip");
+            });
+        } catch(e) {
+            console.log("Error Caught:\n" + e);
+        }
     }
     i++;
 }).on('end', () => {
     console.log("Finished parsing the csv, waiting for the downloads to finish...\n");
 });
-*/
 
-// Create the codeql databases for the repositories
+/* Create the codeql databases for the repositories
 fs.createReadStream('../flask_repos.csv')
   .pipe(csvParser())
   .on('data', (data) => {
@@ -59,7 +62,7 @@ fs.createReadStream('../flask_repos.csv')
             try {
                 exec("codeql database create " + dir + "/" + repo[0] + "-database --language=" + lang.toLowerCase() + " --source-root " + dir + "/" + repo[0], {timeout: 480000});
             } catch(e) {
-                console.log(e);
+                console.log("Error Caught:\n" + e);
             }
         }
     }
@@ -67,3 +70,4 @@ fs.createReadStream('../flask_repos.csv')
 }).on('end', () => {
     console.log("Finished parsing the csv, waiting for the creation of the databases to finish...\n");
 });
+*/
