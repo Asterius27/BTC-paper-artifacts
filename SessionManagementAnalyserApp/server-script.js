681a7eb9486460e62c06bdb535af80c162a756fe
Asterius27 <878352@stud.unive.it>
2023-10-12 11:24:59 +0200

    Update server-script.js

--- SessionManagementAnalyserApp/server-script.js
+++ SessionManagementAnalyserApp/server-script.js
@@ -2,12 +2,13 @@ import { Octokit } from "octokit";
 import 'dotenv/config';
 import * as fs from 'fs';
 import decompress from "decompress";
-import { execSync } from "child_process";
+import { exec } from "child_process";
 import csvParser from 'csv-parser';
 
 const octokit = new Octokit({ auth: process.env.TOKEN });
 const framework = "Flask";
 let i = 0;
+let lang = "python";
 // let skip = [];
 
 if (!fs.existsSync("./repositories")){
@@ -17,6 +18,7 @@ if (!fs.existsSync("./repositories/" + framework)){
     fs.mkdirSync("./repositories/" + framework);
 }
 
+/* Download and extract the repositories
 fs.createReadStream('../flask_repos.csv')
   .pipe(csvParser())
   .on('data', (data) => {
@@ -41,5 +43,27 @@ fs.createReadStream('../flask_repos.csv')
     }
     i++;
 }).on('end', () => {
-    console.log("Finished downloading and extracting the repos\n");
+    console.log("Finished parsing the csv, waiting for the downloads to finish...\n");
+});
+*/
+
+// Create the codeql databases for the repositories
+fs.createReadStream('../flask_repos.csv')
+  .pipe(csvParser())
+  .on('data', (data) => {
+    if (i < 50) {
+        let owner = data.repo_url.split("/")[3];
+        let dir = './repositories/' + framework + '/' + owner + "_" + data.repo_name;
+        let repo = fs.readdirSync(dir);
+        if (repo.length === 1) {
+            try {
+                exec("codeql database create " + dir + "/" + repo[0] + "-database --language=" + lang.toLowerCase() + " --source-root " + dir + "/" + repo[0], {timeout: 480000});
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    }
+    i++;
+}).on('end', () => {
+    console.log("Finished parsing the csv, waiting for the creation of the databases to finish...\n");
 });
