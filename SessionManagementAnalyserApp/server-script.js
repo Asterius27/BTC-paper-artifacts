36a043bccd1055f4c318797c5da4bea69fc87c13
Asterius27 <878352@stud.unive.it>
2023-10-13 15:16:47 +0200

    updated server script

--- SessionManagementAnalyserApp/server-script.js
+++ SessionManagementAnalyserApp/server-script.js
@@ -9,6 +9,7 @@ const octokit = new Octokit({ auth: process.env.TOKEN });
 const framework = "Flask";
 let i = 0;
 let lang = "python";
+let extensions = [".pyx", ".pxd", ".pxi", ".numpy", ".numpyw", ".numsc", ".py", ".cgi", ".fcgi", ".gyp", ".gypi", ".lmi", ".py3", ".pyde", ".pyi", ".pyp", ".pyt", ".pyw", ".rpy", ".spec", ".tac", ".wsgi", ".xpy", ".pytb"];
 // let skip = [];
 
 if (!fs.existsSync("./repositories")){
@@ -22,7 +23,7 @@ process.on('uncaughtException', function (exception) {
     console.log("Error Caught:\n" + exception);
 });
 
-// Download and extract the repositories
+/* Download and extract the repositories
 fs.createReadStream('../flask_repos.csv')
   .pipe(csvParser())
   .on('data', (data) => {
@@ -35,7 +36,6 @@ fs.createReadStream('../flask_repos.csv')
                 'X-GitHub-Api-Version': '2022-11-28'
             }
         }).then(async (zip) => {
-            /*
             fs.appendFileSync("repositories/" + framework + "/" + owner + "_" + data.repo_name + ".zip", Buffer.from(zip.data));
             try {
                 await decompress('./repositories/' + framework + '/' + owner + "_" + data.repo_name + '.zip', './repositories/' + framework + '/' + owner + "_" + data.repo_name);
@@ -44,13 +44,37 @@ fs.createReadStream('../flask_repos.csv')
                 // skip.push(owner + "_" + data.repo_name);
             }
             fs.unlinkSync("repositories/" + framework + "/" + owner + "_" + data.repo_name + ".zip");
-            */
         });
     }
     i++;
 }).on('end', () => {
     console.log("Finished parsing the csv, waiting for the downloads to finish...\n");
 });
+*/
+
+// Remove unnecessary files
+function cleanUpRepos(dir) {
+    let files = fs.readdirSync(dir, { withFileTypes: true }).filter(item => item.isFile()).map(item => item.name);
+    let sub_dirs = fs.readdirSync(dir, { withFileTypes: true }).filter(item => item.isDirectory()).map(item => item.name);
+    if (sub_dirs.length === 0) {
+        for (let i = 0; i < files.length; i++) {
+            console.log(files[i]);
+            if (!extensions.some(e => files[i].endsWith(e))) {
+                fs.unlinkSync(String(dir) + "/" + String(files[i]))
+            }
+        }
+    } else {
+        for (let i = 0; i < files.length; i++) {
+            if (!extensions.some(e => files[i].endsWith(e))) {
+                fs.unlinkSync(String(dir) + "/" + String(files[i]))
+            }
+        }
+        for (let i = 0; i < sub_dirs.length; i++) {
+            cleanUpRepos(dir + "/" + sub_dirs[i]);
+        }
+    }
+}
+cleanUpRepos("repositories/" + framework);
 
 /* Create the codeql databases for the repositories
 fs.createReadStream('../flask_repos.csv')
