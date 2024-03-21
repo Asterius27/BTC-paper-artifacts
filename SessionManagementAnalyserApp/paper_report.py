b188acf43a876aaca5bd4833cf680aee9e4034b3
Asterius27 <878352@stud.unive.it>
2024-03-21 14:04:49 +0100

    finished paper report script

--- SessionManagementAnalyserApp/paper_report.py
+++ SessionManagementAnalyserApp/paper_report.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
 import csv
-import pprint
 
 def loadCSV(csvFile):
     repos = {}
@@ -13,7 +12,7 @@ def loadCSV(csvFile):
             repos[owner + "_" + repo] = row["repo_url"]
     return repos
 
-def countRepos(reposDir, queryDir, queryName, result, csvDict):
+def extractResults(reposDir, queryDir, queryName, result, csvDict):
     output_results = {}
     path = Path(__file__).parent / './repositories'
     repos = os.listdir(str(path.absolute()) + "/" + reposDir)
@@ -44,77 +43,7 @@ def countRepos(reposDir, queryDir, queryName, result, csvDict):
                                 output_results[repo]["result"] += output.read()
     return output_results
 
-def countReposAnd(reposDir, queries, results, csvDict):
-    counter = 0
-    output_results = ""
-    path = Path(__file__).parent / './repositories'
-    repos = os.listdir(str(path.absolute()) + "/" + reposDir)
-    for repo in repos:
-        if os.path.isdir(os.path.join(str(path.absolute()) + "/" + reposDir, repo)):
-            dirs = os.listdir(str(path.absolute()) + "/" + reposDir + "/" + repo)
-            for dir in dirs:
-                if os.path.isdir(os.path.join(str(path.absolute()) + "/" + reposDir + "/" + repo, dir)) and dir.endswith("-results"):
-                    flag = True
-                    temp_output = ""
-                    for i, query in enumerate(queries):
-                        queryFile = str(path.absolute()) + "/" + reposDir + "/" + repo + "/" + dir + "/" + query + ".txt"
-                        if os.path.isfile(queryFile):
-                            with open(queryFile, "r") as output:
-                                if len(output.readlines()) <= 2 and results[i]:
-                                    flag = False
-                                output.seek(0)
-                                if len(output.readlines()) > 2 and not results[i]:
-                                    flag = False
-                                output.seek(0)
-                                if flag:
-                                    output.seek(0)
-                                    temp_output += query.split("/")[1] + ":\n"
-                                    temp_output += output.read()
-                        else:
-                            flag = False
-                    if flag:
-                        counter += 1
-                        if repo in csvDict:
-                            output_results += "URL: " + csvDict[repo] + "\n"
-                        output_results += "File: " + queryFile + "\n"
-                        output_results += temp_output + "\n\n"
-    return counter, output_results
-
-def countReposOr(reposDir, queries, results, csvDict):
-    counter = 0
-    output_results = ""
-    path = Path(__file__).parent / './repositories'
-    repos = os.listdir(str(path.absolute()) + "/" + reposDir)
-    for repo in repos:
-        if os.path.isdir(os.path.join(str(path.absolute()) + "/" + reposDir, repo)):
-            dirs = os.listdir(str(path.absolute()) + "/" + reposDir + "/" + repo)
-            for dir in dirs:
-                if os.path.isdir(os.path.join(str(path.absolute()) + "/" + reposDir + "/" + repo, dir)) and dir.endswith("-results"):
-                    flag = False
-                    temp_output = ""
-                    for i, query in enumerate(queries):
-                        queryFile = str(path.absolute()) + "/" + reposDir + "/" + repo + "/" + dir + "/" + query + ".txt"
-                        if os.path.isfile(queryFile):
-                            with open(queryFile, "r") as output:
-                                if len(output.readlines()) <= 2 and not results[i]:
-                                    flag = True
-                                    output.seek(0)
-                                    temp_output += query.split("/")[1] + ":\n"
-                                    temp_output += output.read()
-                                if len(output.readlines()) > 2 and results[i]:
-                                    flag = True
-                                    output.seek(0)
-                                    temp_output += query.split("/")[1] + ":\n"
-                                    temp_output += output.read()
-                    if flag:
-                        counter += 1
-                        if repo in csvDict:
-                            output_results += "URL: " + csvDict[repo] + "\n"
-                        output_results += "File: " + queryFile + "\n"
-                        output_results += temp_output + "\n\n"
-    return counter, output_results
-
-def saveResults(resultRepos, subDicts):
+def buildResultsDict(resultRepos, subDicts):
     result = {}
     for key in resultRepos:
         flag = False
@@ -132,12 +61,15 @@ def saveResults(resultRepos, subDicts):
     return result
 
 csv_dict = loadCSV(Path(__file__).parent / '../flask_login_final_whitelist_filtered_merged_list.csv')
-# counter, results = countReposAnd("Flask", ["Flask-secret-key/secret_key", "Cookie-name-prefixes/name_prefix_session_cookie"], [False, True], csv_dict)
-# counter, results = countReposOr("Flask", ["Flask-secret-key/secret_key", "Cookie-name-prefixes/name_prefix_session_cookie"], [False, False], csv_dict)
-resultDict1 = countRepos("Flask", "Flask-secret-key", "secret_key", False, csv_dict)
-resultDict2 = countRepos("Flask", "Cookie-name-prefixes", "name_prefix_session_cookie", True, csv_dict)
+resultDict1 = extractResults("Flask", "Flask-secret-key", "secret_key", False, csv_dict)
+resultDict2 = extractResults("Flask", "Cookie-name-prefixes", "name_prefix_session_cookie", True, csv_dict)
+resultDict3 = extractResults("Flask", "Flask-login-session-protection", "session_protection_basic", True, csv_dict)
 keys1 = set(resultDict1)
 keys2 = set(resultDict2)
-intersect = keys1.union(keys2)
-results = saveResults(intersect, [resultDict1, resultDict2])
-pprint.pprint(results)
+keys3 = set(resultDict3)
+intersect = keys1.intersection(keys2)
+union = intersect.union(keys3)
+results = buildResultsDict(union, [resultDict1, resultDict2, resultDict3])
+counter = len(results)
+print(counter)
+print(results)
