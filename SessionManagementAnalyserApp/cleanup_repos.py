691432804c8b3b87f9bad55010761887937236c3
Asterius27 <878352@stud.unive.it>
2024-03-25 15:03:18 +0100

    .

--- SessionManagementAnalyserApp/cleanup_repos.py
+++ SessionManagementAnalyserApp/cleanup_repos.py
@@ -2,6 +2,17 @@ from pathlib import Path
 import csv
 import os
 import shutil
+import sys
+
+maxInt = sys.maxsize
+while True:
+    # decrease the maxInt value by factor 10 
+    # as long as the OverflowError occurs.
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt/10)
 
 csv_path = Path(__file__).parent / '../new_lists/django_filtered.csv'
 repos_path = Path(__file__).parent / './repositories/Django_READMEs'
