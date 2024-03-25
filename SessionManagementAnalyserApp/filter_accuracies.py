ced9a836a95ea1fdfc7617b91a2bf589e6989a98
Asterius27 <878352@stud.unive.it>
2024-03-25 14:59:45 +0100

    .

--- SessionManagementAnalyserApp/filter_accuracies.py
+++ SessionManagementAnalyserApp/filter_accuracies.py
@@ -88,7 +88,7 @@ print("Whitelist Positive Predictive Value (Precision): " + str(getPercentage(tr
 print("Whitelist Negative Predictive Value: " + str(getPercentage(true_negatives_whitelist, true_negatives_whitelist + false_negatives_whitelist)) + " %")
 print("Whitelist F1 Score: " + str(getPercentage(true_positives_whitelist, true_positives_whitelist + 0.5*(false_positives_whitelist + false_negatives_whitelist))) + " %")
 print("\n")
-
+"""
 print(whitelist_filtered_list_positives.intersection(manual_filtered_list_true_positives))
 print("\n")
 print(whitelist_filtered_list_negatives.intersection(manual_filtered_list_true_negatives))
@@ -97,3 +97,4 @@ print(whitelist_filtered_list_positives.difference(whitelist_filtered_list_posit
 print("\n")
 print(whitelist_filtered_list_negatives.difference(whitelist_filtered_list_negatives.intersection(manual_filtered_list_true_negatives)))
 print("\n")
+"""
