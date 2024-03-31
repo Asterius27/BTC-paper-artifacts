e6f700a5cf229c1abd96c916a17beee3d0788090
Asterius27 <878352@stud.unive.it>
2024-03-31 16:37:28 +0200

    Update paper_report.py

--- SessionManagementAnalyserApp/paper_report.py
+++ SessionManagementAnalyserApp/paper_report.py
@@ -115,7 +115,7 @@ def getPercentage(value, total):
         return 0
     return (value / total) * 100
 
-csv_dict = loadCSV(Path(__file__).parent / '../old_lists_with_whitelist_filtering/flask_login_final_whitelist_filtered_merged_list.csv')
+csv_dict = loadCSV(Path(__file__).parent / '../flask_whitelist_filtered_v2.csv')
 flask_login_usage = extractResults("Flask", ".", "flask_library_used_check", True, csv_dict)
 flask_login_required_usage = extractResults("Flask", "Login-restrictions", "un_no_authentication_checks_general", False, csv_dict)
 flask_custom_session_interface = extractResults("Flask", "Explorative-queries", "un_custom_session_interface", False, csv_dict)
