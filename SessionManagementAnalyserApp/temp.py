4c64b434e33fbae865cfed160e89f584f02eeffc
Asterius27 <878352@stud.unive.it>
2024-04-02 14:55:54 +0200

    temp

--- SessionManagementAnalyserApp/temp.py
+++ SessionManagementAnalyserApp/temp.py
@@ -1,19 +1,19 @@
-import csv
 from pathlib import Path
+import os
 
-path1 = Path(__file__).parent / '../old_lists_with_whitelist_filtering/django_final_whitelist_filtered_list.csv'
-path2 = Path(__file__).parent / '../old_lists_with_whitelist_filtering/django_final_whitelist_filtered_list_v2.csv'
+repos_path = Path(__file__).parent / './repositories/Flask'
+counter = 0
 
-def loadCSV(csvFile):
-    repos = []
-    with csvFile.open(encoding="utf8") as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            repos.append(row["repo_url"])
-    return set(repos)
-
-old_list = loadCSV(path1)
-new_list = loadCSV(path2)
-print(len(old_list.intersection(new_list)))
-print(len(new_list.difference(old_list)))
-print(len(old_list.difference(new_list)))
+repos_dir = os.listdir(repos_path.absolute())
+for dir_name in repos_dir:
+    subdirs = os.listdir(str(repos_path.absolute()) + "/" + dir_name)
+    for subdir in subdirs:
+        if subdir.endswith("-results"):
+            queryFile = str(repos_path.absolute()) + "/" + dir_name + "/" + subdir + "/flask_library_used_check.txt"
+            with open(queryFile, "r") as output:
+                if len(output.readlines()) > 2:
+                    with open(str(repos_path.absolute()) + "/" + dir_name + "/" + subdir + "/info.txt", 'a') as res:
+                        res.write(", flask")
+                        print(str(repos_path.absolute()) + "/" + dir_name + "/" + subdir)
+                        counter += 1
+print(counter)
